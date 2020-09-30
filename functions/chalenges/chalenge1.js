4ea83504171cf099c9879829f927e770b137500b
eurico77 <euricomagalhaes61@gmail.com>
2020-09-29 22:22:27 -0300

    concluindo desafio de funções

--- functions/chalenges/chalenge1.js
+++ functions/chalenges/chalenge1.js
@@ -1,5 +1,3 @@
-// criar uma func somar (3)(4)(5)
-
 function somar(a) {
     return function (b) {
         return function (c) {
@@ -9,3 +7,18 @@ function somar(a) {
 }
 
 // calcular(3)(7)(fn que ira calcular os valores anteriores)
+
+function calcular(p) {
+    return function (x) {
+        return function (fn) {
+            return fn(p, x);
+        };
+    };
+}
+
+const subtrair = (a, b) => a - b;
+const multiplicar = (a, b) => a * b;
+
+const data = calcular(12)(10)(multiplicar);
+
+console.log(` o resultado é:  ${data} `);
